097351c2c03814b949955f6c508ecf9eafd72aa6
Indranil33 <indranilganguly012@gmail.com>
2025-11-16 14:15:10 +0530

    Change file

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();   
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
@@ -22,7 +23,7 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/blogCMS', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
